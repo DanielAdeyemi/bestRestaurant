2c572f1922b575fe95ac289b1cb7746e2b62394b
Mikey Kriegel <mikkrieg@gmail.com>
2021-03-11 15:25:52 -0800

    Add displayname property to numberofemployees
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

--- RestaurantFinder/Models/Restaurant.cs
+++ RestaurantFinder/Models/Restaurant.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+
 namespace RestaurantFinder.Models
 {
     public class Restaurant
@@ -6,6 +8,7 @@ namespace RestaurantFinder.Models
         public string Description { get; set; }
         public int RestaurantId { get; set; }
         public string Hours { get; set; }
+        [DisplayName("Number of Employees")]
         public int NumberOfEmployees { get; set; }
         public int CuisineId { get; set; }
         public virtual Cuisine Cuisine { get; set; }
